b6b930713009c43fa6d5ab2e7c73883b9d992844
hassansyed1029 <syedhassan1029@gmail.com>
2024-03-19 22:52:14 -0400

    created this

--- app/ui/ProspectTable.js
+++ app/ui/ProspectTable.js
@@ -1,31 +1,31 @@
 
-export default function ProspectTable({ url, button = false }) {
+export default function ProspectTable() {
     return (
         <div className="min-w-full h-[500px] border-2 border-black-500/75 shadow-2xl rounded-lg ">
-            <div class="relative overflow-x-auto">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-black">
-                    <thead class="text-xs text-gray-700  dark:text-gray-400">
+            <div className="relative overflow-x-auto">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-black">
+                    <thead className="text-xs text-gray-700  dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-6 py-3 text-black">
+                            <th scope="col" className="px-6 py-3 text-black">
                                 Search name
                             </th>
-                            <th scope="col" class="px-6 py-3 text-black">
+                            <th scope="col" className="px-6 py-3 text-black">
                                 Date
                             </th>
-                            <th scope="col" class="px-6 py-3 text-black">
+                            <th scope="col" className="px-6 py-3 text-black">
                                 View Results
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="border rounded-lg">
-                            <th scope="row" class="px-6 py-4 font-medium">
+                        <tr className="border rounded-lg">
+                            <th scope="row" className="px-6 py-4 font-medium">
                                 Approov Security
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 2/19/24
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 <button
                                     type="button"
                                     className="rounded-md bg-indigo-50 px-3.5 py-2.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100"
@@ -34,14 +34,14 @@ export default function ProspectTable({ url, button = false }) {
                                 </button>
                             </td>
                         </tr>
-                        <tr class="border rounded-lg">
-                            <th scope="row" class="px-6 py-4 font-medium">
+                        <tr className="border rounded-lg">
+                            <th scope="row" className="px-6 py-4 font-medium">
                                 Google
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 2/21/24
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 <button
                                     type="button"
                                     className="rounded-md bg-indigo-50 px-3.5 py-2.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100"
@@ -50,14 +50,14 @@ export default function ProspectTable({ url, button = false }) {
                                 </button>
                             </td>
                         </tr>
-                        <tr class="border rounded-lg">
-                            <th scope="row" class="px-6 py-4 font-medium">
+                        <tr className="border rounded-lg">
+                            <th scope="row" className="px-6 py-4 font-medium">
                                 Vanta
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 2/23/24
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 <button
                                     type="button"
                                     className="rounded-md bg-indigo-50 px-3.5 py-2.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100"
