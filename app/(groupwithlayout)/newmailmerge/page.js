9c7fafeb90bcb1bc70c8f9017666b4e3d000c318
hassansyed1029 <syedhassan1029@gmail.com>
2024-06-02 14:24:51 -0400

    got all animations working well

--- app/(groupwithlayout)/newmailmerge/page.js
+++ app/(groupwithlayout)/newmailmerge/page.js
@@ -7,9 +7,9 @@ import AIMailMerge from '@/app/ui/AiMailMerge'
 export default async function NewMailMerge() {
     const data = await grabEmailSequences()
     return (
-    <div className="min-w-full">
-        <Navbar url="New AI Mail Merge" />
-        <AIMailMerge className="bg-red-100"data={data}/>
-    </div>
+        <div className="min-w-full">
+            <Navbar url="New AI Mail Merge" />
+            <AIMailMerge className="bg-red-100" data={data} />
+        </div>
     );
 }
