60183db9a11fbb50c543e7cd82936bbf9982d672
hassansyed1029 <syedhassan1029@gmail.com>
2024-06-10 15:47:39 -0400

    fixing parsing issue

--- app/lib/teststep.js
+++ app/lib/teststep.js
@@ -127,6 +127,8 @@ function removeOuterQuotes(str) {
     }
     if(str.charAt(0) === "\"" && str.charAt(str.length-1) === "\"") {
         return str.slice(1,-1)
+    } else {
+        return str;
     }
 }
 
